3e7f44c5214e2ffac9060ff22a8df408aaebd7c3
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2023-09-06 18:52:04 +0300

    custom navigation bar

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -22,8 +22,25 @@ class HomeViewController: UIViewController {
         
         HomeFeedTableView.delegate = self
         HomeFeedTableView.dataSource = self
+        
+        configureNavbar()
+        
+        
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         HomeFeedTableView.tableHeaderView = headerView
+    }
+    
+    private func configureNavbar() {
+        var image = UIImage(named: "logo_netflix")
+        image = image?.withRenderingMode(.alwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+        navigationController?.navigationBar.tintColor = .white
+        
+        
     }
     
     override func viewDidLayoutSubviews() {
@@ -53,5 +70,11 @@ extension HomeViewController: ConfigureTableView{
     }
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let contentOffsety = scrollView.contentOffset.y
+        let offset = scrollView.contentOffset.y + defaultOffset
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
     }
 }
