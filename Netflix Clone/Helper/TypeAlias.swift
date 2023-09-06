457bddaaaccaef976156fb4a77c214f1fcea1b92
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2023-09-06 15:17:04 +0300

    setting home TableViewCell and it's CollectionView

---
+++ Netflix Clone/Helper/TypeAlias.swift
@@ -0,0 +1,12 @@
+//
+//  TypeAlias.swift
+//  Netflix Clone
+//
+//  Created by Kadir Hocaoğlu on 6.09.2023.
+//
+
+import Foundation
+import UIKit
+
+typealias ConfigureTableView = UITableViewDelegate & UITableViewDataSource
+typealias ConfigureCollectionView = UICollectionViewDelegate & UICollectionViewDataSource
