01f3d5d0e14a68cfb81f8b262f96e72dca1f5055
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2023-09-06 12:53:04 +0300

    create app

---
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Kadir Hocaoğlu on 5.09.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Kadir Hocaoğlu on 5.09.2023.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
